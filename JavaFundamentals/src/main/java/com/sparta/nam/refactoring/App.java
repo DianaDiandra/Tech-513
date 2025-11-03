3eedf910ad8afd777837e2c87991cf84ce31771f
unknown <nish_mandal@hotmail.com>
2025-11-03 15:26:03 +0000

    Refactoring lesson complete

--- JavaFundamentals/src/main/java/com/sparta/nam/refactoring/App.java
+++ JavaFundamentals/src/main/java/com/sparta/nam/refactoring/App.java
@@ -2,13 +2,13 @@ package com.sparta.nam.refactoring;
 
 public class App {
     public static void main(String[] args) {
-        int timeOfDay = 21;
+        int timeOfDay = 12;
         String result = getGreeting(timeOfDay);
         System.out.println(result);
     }
 
     public static String getGreeting(int timeOfDay) {
-        if(timeOfDay >= 5 && timeOfDay <=12){
+        if(timeOfDay >= 5 && timeOfDay <12){
             return "Good morning!";
         } else if(timeOfDay >=12 && timeOfDay <=18){
             return "Good afternoon!";
--- JavaFundamentals/src/test/java/com/sparta/nam/refactoring/AppTest.java
+++ JavaFundamentals/src/test/java/com/sparta/nam/refactoring/AppTest.java
@@ -3,6 +3,9 @@ package com.sparta.nam.refactoring;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class AppTest {
 
@@ -49,5 +52,49 @@ public class AppTest {
     {
         Assertions.assertEquals("Good evening!", App.getGreeting(2));
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5,11}) // supported numbers, characters, booleans, strings and classes
+    @DisplayName("getGreeting, when given a time from 5 to 11, returns good morning")
+    public void getGreeting_GivenATimeFrom5To11_ReturnsGoodMorning(int time){
+        Assertions.assertEquals("Good morning!", App.getGreeting(time));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Good evening!, 2",
+            "Good morning!, 8",
+            "Good afternoon!, 15",
+            "Good evening!, 21"
+    })
+    @DisplayName("getGreeting, when given a time, returns appropriate greeting")
+    public void givenATime_Greeting_ReturnsAnAppropriateGreeting(String greeting, int time){
+        Assertions.assertEquals(greeting, App.getGreeting(time));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {12, 18})
+    @DisplayName("getGreeting, when given a time from 12 to 18, returns good afternoon")
+    public void getGreeting_GivenATimeFrom12To18_ReturnsGoodAfternoon(int time)
+    {
+        Assertions.assertEquals("Good afternoon!", App.getGreeting(time));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 4})
+    @DisplayName("getGreeting, when given a time from 0 to 4, returns good evening")
+    public void getGreeting_GivenATimeFrom0To4_ReturnsGoodEvening(int time)
+    {
+        Assertions.assertEquals("Good evening!", App.getGreeting(time));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {19, 23})
+    @DisplayName("getGreeting, when given a time from 19 to 23, returns good evening")
+    public void getGreeting_GivenATimeFrom19To23_ReturnsGoodEvening(int time)
+    {
+        Assertions.assertEquals("Good evening!", App.getGreeting(time));
+    }
+
 
 }
